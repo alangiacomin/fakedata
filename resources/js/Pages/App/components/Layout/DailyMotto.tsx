c4a9c6d1b55bcfd287d75fd2048c0d2f431abf22
Alan Giacomin <alan.giacomin@gmail.com>
2026-04-21 22:52:41 +0200

    motto del giorno pt.3

--- resources/js/Pages/App/components/Layout/DailyMotto.tsx
+++ resources/js/Pages/App/components/Layout/DailyMotto.tsx
@@ -9,7 +9,7 @@ const DailyMotto: FC<DailyMottoProps> = ({motto}: DailyMottoProps): ReactNode =>
         <section className="bg-body-tertiary border-top py-3" aria-label="Motto del giorno">
             <div className="container text-center">
                 <p className="mb-0 text-body-secondary">
-                    <span className="fw-semibold text-body">Motto del momento:</span> {motto}
+                    <span className="fw-semibold text-body">Pensiero senza fronzoli:</span> {motto}
                 </p>
             </div>
         </section>
